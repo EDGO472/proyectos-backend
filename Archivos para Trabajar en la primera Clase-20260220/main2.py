1cfee807984f12d83173096daa2398d90cf4628a
Edwin Gerardo Gonzalez Gamboa <edwgonzalez95@gmail.com>
2026-03-05 21:26:23 -0600

    Finalizadas Partes 10 y 11: Modelos Pydantic y Parámetros Opcionales

--- Archivos para Trabajar en la primera Clase-20260220/main2.py
+++ Archivos para Trabajar en la primera Clase-20260220/main2.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()  
+
+class Producto(BaseModel):
+    id: int
+    nombre: str
+    precio: float
+    en_stock: bool = True  # Valor por defecto
+
+
     #parametros
 @app.get("/")    #funcion decoradora(resive una funcion)
 def prueba():
@@ -13,3 +23,24 @@ def path_wraper(id:int):
 @app.get("/multiples/path/{id}/{nombre}/{edad}")
 def multiples_path_wraper(id:int, nombre:str, edad:int):
     return{"Usuario":id,"nombre":nombre,"edad":edad}
+
+@app.post("/productos/")
+def crear_producto(producto: Producto):
+    return {"mensaje": "Producto guardado", "data": producto}
+
+# Importa Optional desde typing (agrégalo al inicio de tu archivo)
+from typing import Optional
+
+# ... (tus clases y funciones anteriores se quedan igual)
+
+@app.get("/items/")
+def leer_items(nombre: str, precio: float, impuesto: Optional[float] = None):
+    # 'nombre' y 'precio' son obligatorios.
+    # 'impuesto' es OPCIONAL (si no lo mandan, vale None).
+    
+    resultado = {"item_nombre": nombre, "item_precio": precio}
+    
+    if impuesto:
+        resultado.update({"precio_total": precio + impuesto})
+        
+    return resultado
